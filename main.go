f061e5308097bf53865a85e234231eca6f4532a7
Ocquidant Nicolas <nocquidant@voyages-sncf.com>
2018-04-12 10:56:42 +0200

    Call backend

--- main.go
+++ main.go
@@ -1,20 +1,63 @@
 package main
+
 import (
-        "strconv"
-        "fmt"
-        "net/http"
-        "os"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
 )
-func handler(w http.ResponseWriter, r *http.Request) {
-        h, _ := os.Hostname()
-        fmt.Fprintf(w, "Hi there, I'm served from %s!", h)
+
+func handlerHello(w http.ResponseWriter, r *http.Request) {
+	h, _ := os.Hostname()
+	fmt.Fprintf(w, "Hi there, I'm served from %s!\n", h)
 }
+
+func handlerCallBack(w http.ResponseWriter, r *http.Request) {
+	url := "http://go-hello-back:8485/hello"
+
+	// Build the request
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal("NewRequest: ", err)
+		return
+	}
+
+	// A Client is an HTTP client
+	client := &http.Client{}
+
+	// Send the request via a client
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Do: ", err)
+		return
+	}
+
+	// Callers should close resp.Body
+	defer resp.Body.Close()
+
+	// Get body as string
+	if resp.StatusCode == http.StatusOK {
+		bodyBytes, err2 := ioutil.ReadAll(resp.Body)
+		if err2 != nil {
+			log.Fatal("Do: ", err)
+			return
+		}
+		fmt.Fprintf(w, "Got response from the back. %s\n", string(bodyBytes))
+	}
+
+	fmt.Fprintf(w, "Error while calling the back: %d", resp.StatusCode)
+}
+
 func main() {
-        port := 8484
-        ep := "hello"
+	port := 8484
 
-        fmt.Fprintf(os.Stdout, "Running HTTP server, using endpoint: %s, port: %d", ep, port)
+	fmt.Fprintf(os.Stdout, "HTTP server is running using port: %d\n", port)
+	fmt.Fprintf(os.Stdout, "Available endpoints are: '/hello' and '/call'")
 
-        http.HandleFunc("/" + ep, handler)
-        http.ListenAndServe(":" + strconv.Itoa(port), nil)
+	http.HandleFunc("/hello", handlerHello)
+	http.HandleFunc("/call", handlerCallBack)
+
+	http.ListenAndServe(":"+strconv.Itoa(port), nil)
 }
