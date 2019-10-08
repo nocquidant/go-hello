83a9013fafd14a1c23df4e1d1458bd954ec8b3bb
Nicolas Ocquidant <nocquidant@oui.sncf>
2019-10-08 14:10:19 +0200

    ci : install bin in local bin

--- scripts/installMage.sh
+++ scripts/installMage.sh
@@ -1,9 +1,10 @@
 mage_version="1.9.0"
 mage_url="https://github.com/magefile/mage/releases/download/v${mage_version}/mage_${mage_version}_Linux-64bit.tar.gz"
 
-curl -L -s -o mage.tar.gz ${mage_url} 
+mkdir -p ~/bin && curl -L -s -o ~/bin/mage.tar.gz ${mage_url} 
 if [ $? -ne 0 ]; then exit 1; fi
 
-tar xzf mage.tar.gz
-cp mage /usr/local/bin/mage
-chmod +x /usr/local/bin/mage
+tar xzf ~/bin/mage.tar.gz -C ~/bin && chmod +x ~/bin/mage
+export PATH=$PATH:~/bin
+
+mage --version
